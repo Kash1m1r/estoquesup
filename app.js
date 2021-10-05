d59be867a118c3f902f0f2ec32d243ea5782238f
Gabriel Pereira Gomes <adm1nkash1m1r@gmail.com>
2021-10-05 13:50:16 -0300

    use validation

--- app.js
+++ app.js
@@ -6,7 +6,22 @@ const admin = require('./routes/admin');
 const path = require('path');
 const { extname } = require("path");
 const mongoose = require("mongoose");
+const session = require('express-session');
+const flash = require('connect-flash');
 // Configurações
+    //session
+    app.use(session({
+        secret: "sup0rt3",
+        resave: true,
+        saveUninitialized: true
+    }))
+    app.use(flash())
+    //middleware
+    app.use((req, res, next) => {
+        res.locals.success_msg = req.flash('success_msg');
+        res.locals.error_msg = req.flash('error_msg');
+        next();
+    })
     // Body-Parser
     app.use(bodyParser.urlencoded({extended: true}))
     app.use(bodyParser.json())
@@ -16,7 +31,6 @@ const mongoose = require("mongoose");
     // Mongoose
     mongoose.connect("mongodb://localhost/estoquesup").then(() => {
         console.log("..Conexão com o banco estabelicida com sucesso!");
-        console.log("..Conexão com o banco estabelecida com sucesso!");
     }).catch((err) => {
         console.log("Erro ao se comunicar com o banco de dados"+err);
     })
--- routes/admin.js
+++ routes/admin.js
@@ -18,18 +18,47 @@ router.get('/listarequip', (req, res) => {
 });
 
 router.post("/listarequip/add", (req, res) => {
-    const newEquip = {
-        equipamento: req.body.equip,
-        quantidade: req.body.quantd,
-        marca: req.body.marca,
-        modelo: req.body.modelo
+
+    const erros = [];
+
+    if(!req.body.equip || typeof req.body.equip == undefined || req.body.equip == null || req.body.equip == Number){
+        erros.push({texto: "Equipamento inválido!"});
+    }
+    if(!req.body.quantd || typeof req.body.quantd == undefined || req.body.quantd == null){
+        erros.push({texto: "Quantidade inválida!"});
+    }
+    if(typeof req.body.quantd == 'number'){
+        erros.push({texto: "Quantidade errada"});
+    }
+    if(!req.body.marca || typeof req.body.marca == undefined || req.body.marca == null || req.body.marca == Number){
+        erros.push({texto: "Marca inválida!"});
+    }
+    if(!req.body.modelo || typeof req.body.modelo == undefined || req.body.modelo == null){
+        erros.push({texto: "Modelo inválido!"});
+    }
+    if(req.body.quantd = 0 || req.body.quantd < 0){
+        erros.push({texto: "Quantidade nula ou inferior a zero"});
+    }
+    if(erros.length > 0 ){
+        res.render("admin/cadastrarequip", {erros: erros});
+    }else{
+        const newEquip = {
+            equipamento: req.body.equip,
+            quantidade: req.body.quantd,
+            marca: req.body.marca,
+            modelo: req.body.modelo
+        }
+    
+        new Equip(newEquip).save().then(() =>{
+            req.flash("success_msg", "Equipamento cadastrado com sucesso!");
+            res.redirect('/admin/listarequip');
+        }).catch((err) => {
+            req.flash("error_msg", "Erro ao salvar equipamento");
+            console.log("O equipamento não foi cadastrado"+err);
+        });
     }
 
-    new Equip(newEquip).save().then(() =>{
-        console.log("Equipamento cadastrado com sucesso!");
-    }).catch((err) => {
-        console.log("O equipamento não foi cadastrado"+err);
-    });
+
 })
 
 module.exports = router;
